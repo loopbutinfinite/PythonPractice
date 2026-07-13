8f65ba1cf088e217fe5970cae8458cacb79b95ee
loopbutinfinite <bestrada8800@gmail.com>
2026-07-12 23:43:14 -0700

    Quick intro to Pytho language

---
+++ Intro.py
@@ -0,0 +1,13 @@
+# Python uses new lines to complete a command. This is very different from other languages that ends a command with semicolons or parantheses.
+
+# Indentation is specifically important in Python as it indicates a block of code in Python
+
+#Python Indentation
+
+# No indentation (This will throw an error)
+if 5 > 2:
+print("Five is greater than two!")
+
+# With Indentation
+if 5 > 2:
+	print("Five is greater than two!")
